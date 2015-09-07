35347132b3df49350ed5b9fabf33271b5ddaa26c
SkySom <skylar.som@gmail.com>
2015-09-07 00:34:47 -0400

    Gradle stuff and main instance file

---
+++ src/main/java/moarcarts/MoarCarts.java
@@ -0,0 +1,33 @@
+package moarcarts;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "moarcarts", name = "MoarCarts", version = "1.0.0")
+public class MoarCarts
+{
+	@Instance("moarcarts")
+	public static MoarCarts instance;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+
+	}
+
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+
+	}
+}
