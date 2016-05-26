c492c4eebc3fd8f6021ffb2049992dbdf9e56d58
Skylar Sommers <ssommers@criticalmix.com>
2016-05-26 00:58:34 -0400

    Added Missing recipe

--- src/main/java/xyz/brassgoggledcoders/moarcarts/mods/tinkers/blocks/BlockCartFaucet.java
+++ src/main/java/xyz/brassgoggledcoders/moarcarts/mods/tinkers/blocks/BlockCartFaucet.java
@@ -1,16 +1,25 @@
 package xyz.brassgoggledcoders.moarcarts.mods.tinkers.blocks;
 
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.ShapelessRecipes;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
+import slimeknights.tconstruct.smeltery.TinkerSmeltery;
 import slimeknights.tconstruct.smeltery.block.BlockFaucet;
 import xyz.brassgoggledcoders.boilerplate.lib.client.models.IHasModel;
 import xyz.brassgoggledcoders.boilerplate.lib.common.blocks.IHasItemBlock;
 import xyz.brassgoggledcoders.boilerplate.lib.common.blocks.IHasTileEntity;
+import xyz.brassgoggledcoders.boilerplate.lib.common.items.IHasRecipe;
 import xyz.brassgoggledcoders.moarcarts.MoarCarts;
 import xyz.brassgoggledcoders.moarcarts.mods.tinkers.tiles.TileCartFaucet;
 
-public class BlockCartFaucet extends BlockFaucet implements IHasTileEntity, IHasModel, IHasItemBlock
+import java.util.ArrayList;
+import java.util.List;
+
+public class BlockCartFaucet extends BlockFaucet implements IHasTileEntity, IHasModel, IHasItemBlock, IHasRecipe
 {
 	public BlockCartFaucet()
 	{
@@ -41,5 +50,14 @@ public class BlockCartFaucet extends BlockFaucet implements IHasTileEntity, IHas
 	public Class<? extends ItemBlock> getItemBlockClass()
 	{
 		return ItemBlock.class;
+	}
+
+	@Override
+	public IRecipe[] getRecipes()
+	{
+		List<ItemStack> items = new ArrayList<>();
+		items.add(new ItemStack(Items.minecart));
+		items.add(new ItemStack(TinkerSmeltery.faucet));
+		return new IRecipe[] {new ShapelessRecipes(new ItemStack(this), items)};
 	}
 }
