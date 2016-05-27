d6fcd7f9d4aea78b1c7f2debc680467f331d69d5
Skylar Sommers <ssommers@criticalmix.com>
2016-05-26 23:02:53 -0400

    Almost done with blockstates
    Just movin' stuff and redstone.

--- src/main/java/xyz/brassgoggledcoders/moarcarts/mods/rails/RailsModule.java
+++ src/main/java/xyz/brassgoggledcoders/moarcarts/mods/rails/RailsModule.java
@@ -1,12 +1,24 @@
 package xyz.brassgoggledcoders.moarcarts.mods.rails;
 
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import xyz.brassgoggledcoders.boilerplate.lib.common.modules.Module;
+import xyz.brassgoggledcoders.boilerplate.lib.common.registries.BlockRegistry;
+import xyz.brassgoggledcoders.moarcarts.mods.rails.blocks.BlockRailBoarding;
 
 public class RailsModule extends Module
 {
+	public static BlockRailBoarding BOARDING_RAIL;
+
 	@Override
 	public String getName()
 	{
 		return "Rails";
+	}
+
+	@Override
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		BOARDING_RAIL = new BlockRailBoarding();
+		BlockRegistry.registerBlock(BOARDING_RAIL);
 	}
 }
